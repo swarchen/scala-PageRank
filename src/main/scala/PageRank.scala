02070d355859d5918f7553fea2ebc3d8fd7c1549
jhaocheng.wu <jhaocheng.wu@gmail.com>
2016-05-14 16:14:18 +0800

    ok for no default rank

--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -16,15 +16,18 @@ object PageRank {
 
         val lines = sc.textFile(files)
         val counts = lines.flatMap (line => {
-            val titlePattern = "<title>(.+?)</title>".r
-            val linkPattern = "\\[\\[(.+?)([\\|#]|\\]\\])".r            
-            val title = titlePattern.findFirstMatchIn(line).get.group(1)
+            val titlePattern = "<title>(.+?)</title>".r //define regex of title string out from <title> tag
+            val linkPattern = "\\[\\[(.+?)([\\|#]|\\]\\])".r  //define regex of link string out from [[]] area          
+            val title = titlePattern.findFirstMatchIn(line).get.group(1) // find title by defined regex
+
+            //find all link in pages and do map reduce
             linkPattern.findAllIn(line).matchData.map(link => {
               val corlink = link.group(1).replaceAll("&lt;", "<").replaceAll("&gt;", ">").replaceAll("&amp;", "&").replaceAll("&quot;", "\"").replaceAll("&apos;", "\'")
               val cortitle = title.replaceAll("&lt;", "<").replaceAll("&gt;", ">").replaceAll("&amp;", "&").replaceAll("&quot;", "\"").replaceAll("&apos;", "\'")
               (cortitle -> corlink)
             })               
-        }).reduceByKey(_ +","+ _) 
+        }).reduceByKey(_ ++ _)
+        
         counts.saveAsTextFile(outputPath) // Output
         sc.stop
     }
