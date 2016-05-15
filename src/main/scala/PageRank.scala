5295765ac377ed13b4e0cdd2d00ceffb2bc270f8
jhaocheng.wu <jhaocheng.wu@gmail.com>
2016-05-15 14:48:46 +0800

    while ok

--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -13,9 +13,8 @@ object PageRank {
         val hadoopConf = sc.hadoopConfiguration
         val hdfs = FileSystem.get(hadoopConf)
         try { hdfs.delete(new Path(outputPath), true) } catch { case _ : Throwable => { } }
-
         val lines = sc.textFile(files)
-        val counts = lines.flatMap (line => {
+        val links = lines.flatMap (line => {
             val titlePattern = "<title>(.+?)</title>".r //define regex of title string out from <title> tag
             val linkPattern = "\\[\\[(.+?)([\\|#]|\\]\\])".r  //define regex of link string out from [[]] area          
             val title = titlePattern.findFirstMatchIn(line).get.group(1) // find title by defined regex
@@ -26,9 +25,22 @@ object PageRank {
               val cortitle = title.replaceAll("&lt;", "<").replaceAll("&gt;", ">").replaceAll("&amp;", "&").replaceAll("&quot;", "\"").replaceAll("&apos;", "\'")
               (cortitle -> corlink)
             })               
-        }).reduceByKey(_ ++ _)
-        
-        counts.saveAsTextFile(outputPath) // Output
+        }).groupByKey.cache()
+        val totalNum = links.count()
+        var ranks = links.mapValues (v => 0.01)
+        var oldPR = 0.0
+        var newPR = 1.0
+        while ((newPR - oldPR).abs > 0.001){
+        //for(i <- 1 to 22){
+            oldPR = ranks.map(_._2).sum
+            val contributions = links.join(ranks).flatMap{
+                case(url, (links, rank)) => links.map(dest => (dest, rank / links.size))
+            }
+            ranks = contributions.reduceByKey((x, y) => x+ y).mapValues(v => 0.15 / totalNum + 0.85*v)
+            newPR = ranks.map(_._2).sum
+        }
+        val result = ranks.sortBy{ case(url,pr) => (- pr, url) }
+        result.saveAsTextFile(outputPath) // Output
         sc.stop
     }
 }
